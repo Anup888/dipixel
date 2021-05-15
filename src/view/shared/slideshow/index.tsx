6830d7e92fedbe87f0e3e0e17a8d684c9bdffbf9
anup <anup.gupta@armentum.co>
2021-05-15 16:35:07 +0530

    home page css

--- src/view/shared/slideshow/index.tsx
+++ src/view/shared/slideshow/index.tsx
@@ -10,7 +10,13 @@ import Project3 from "assets/images/home/project3.png";
 export const Slideshow = (props: any) => {
   const images = [Project1, Project2, Project3];
   return (
-    <AliceCarousel autoPlay autoPlayInterval={3000}>
+    <AliceCarousel
+      autoPlay
+      autoPlayInterval={1000}
+      infinite={true}
+      disableDotsControls={true}
+      disableButtonsControls={true}
+    >
       {images?.map((item: any, index: any) => (
         <div key={index} className="each-slide">
           <img src={item} className="sliderimg" alt="imgslide" />
